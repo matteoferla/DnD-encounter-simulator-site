4c9ca053883a4bc4d949f1045e6b0f3482f9f546
Matteo <matteo.ferla@gmail.com>
2015-08-15 20:58:23 +0100

    2 try

--- wsgi.py
+++ wsgi.py
@@ -30,28 +30,29 @@ def application(environ, start_response):
         status = '200 OK'
         headers = [('Content-type', 'text/plain')]
         start_response(status, headers)
-        return [response_body+" too"]
-
-
-    ctype = 'text/plain'
-    if environ['PATH_INFO'] == '/health':
-        response_body = "1"
-    elif environ['PATH_INFO'] == '/env':
-        response_body = ['%s: %s' % (key, value)
-                    for key, value in sorted(environ.items())]
-        response_body = '\n'.join(response_body)
+        return [response_body]
     else:
-        ctype = 'text/html'
-        h=open("static.html")
-        response_body = h.read()
 
-    response_body = response_body.encode('utf-8')
 
-    status = '200 OK'
-    response_headers = [('Content-Type', ctype), ('Content-Length', str(len(response_body)))]
-    #
-    start_response(status, response_headers)
-    return [response_body ]
+        ctype = 'text/plain'
+        if environ['PATH_INFO'] == '/health':
+            response_body = "1"
+        elif environ['PATH_INFO'] == '/env':
+            response_body = ['%s: %s' % (key, value)
+                        for key, value in sorted(environ.items())]
+            response_body = '\n'.join(response_body)
+        else:
+            ctype = 'text/html'
+            h=open("static.html")
+            response_body = h.read()
+
+        response_body = response_body.encode('utf-8')
+
+        status = '200 OK'
+        response_headers = [('Content-Type', ctype), ('Content-Length', str(len(response_body)))]
+        #
+        start_response(status, response_headers)
+        return [response_body ]
 
 #
 # Below for testing only
