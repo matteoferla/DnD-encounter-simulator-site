092096fac32870f5647450e1bc2151010e7a36cd
Matteo <matteo.ferla@gmail.com>
2015-08-15 13:39:10 +0100

    first play around

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='D&D battle simulator',
       version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
+      description='An app to simulate a D&D battle',
+      author='Matteo Ferla',
+      author_email='matteo.ferla@gmail.com',
       url='http://www.python.org/sigs/distutils-sig/',
 #      install_requires=['Django>=1.3'],
      )
