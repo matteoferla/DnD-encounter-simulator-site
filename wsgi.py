4b7a242be8ee5f1cbee25ee1430fad949efedd63
Matteo <matteo.ferla@gmail.com>
2015-08-15 22:27:48 +0100

    back a few steps

--- wsgi.py
+++ wsgi.py
@@ -3,28 +3,48 @@ import os
 
 def application(environ, start_response):
 
-    ctype = 'text/plain'
-    if environ['PATH_INFO'] == '/health':
-        response_body = "1"
-    elif environ['PATH_INFO'] == '/env':
-        response_body = ['%s: %s' % (key, value)
-                    for key, value in sorted(environ.items())]
-        response_body = '\n'.join(response_body)
-    else:
-        ctype = 'text/html'
-        os.chdir("app-root/repo")
+    if environ['REQUEST_METHOD'] == 'POST':              #If POST...
+        #from cgi import parse_qs
         try:
-            h=open("static.html")
-            response_body = h.read()
-            response_body = response_body.encode('utf-8')
-        except Exception as e:
-            response_body = str(e)+"<br/>"+str(os.listdir())
+            request_body_size = int(environ['CONTENT_LENGTH'])
+            request_body = environ['wsgi.input'].read(request_body_size)
+        except (TypeError, ValueError):
+            request_body = "0"
 
-    status = '200 OK'
-    response_headers = [('Content-Type', ctype), ('Content-Length', str(len(response_body)))]
-    #
-    start_response(status, response_headers)
-    return [response_body ]
+        #parsed_body = parse_qs(request_body)
+        #value = parsed_body.get('test_text', [''])[0] #Returns the first value
+
+        try:
+            response_body = request_body+' back'
+        except:
+            response_body = 'error'
+
+        status = '200 OK'
+        headers = [('Content-type', 'text/plain')]
+        start_response(status, headers)
+        return [response_body]
+    else:
+
+
+        ctype = 'text/plain'
+        if environ['PATH_INFO'] == '/health':
+            response_body = "1"
+        elif environ['PATH_INFO'] == '/env':
+            response_body = ['%s: %s' % (key, value)
+                        for key, value in sorted(environ.items())]
+            response_body = '\n'.join(response_body)
+        else:
+            ctype = 'text/html'
+            h=open("app-root/repo/static.html")
+            response_body = h.read()
+
+        response_body = response_body.encode('utf-8')
+
+        status = '200 OK'
+        response_headers = [('Content-Type', ctype), ('Content-Length', str(len(response_body)))]
+        #
+        start_response(status, response_headers)
+        return [response_body ]
 
 #
 # Below for testing only
