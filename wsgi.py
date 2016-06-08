9e7ba882c2befc6007ab4fa8acb8ab599fac5897
Matteo Ferla <matteo.ferla@gmail.com>
2016-06-08 22:02:53 +0200

    fixed

--- wsgi.py
+++ wsgi.py
@@ -11,6 +11,7 @@ else:
     place="server"
     host=8051
     apppath="app-root/repo/"
+print(place)
 DnD.Creature.beastiary=DnD.Creature._beastiary(apppath+'beastiary.csv')
 
 def application(environ, start_response):
